937d7faf12a8ae51f52bcddcb038d99159cae0e9
Ethan Diedericks <diedericksethan@gmail.com>
2024-01-08 23:16:34 +0200

    updated model

--- budget/models.py
+++ budget/models.py
@@ -3,105 +3,31 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 
-class IncomeSource(models.Model):
+class Transaction(models.Model):
     """
-    Model representing the income source for a user.
+    Model representing transactions for a user.
     """
+
+    TRANSACTION_CHOICES = [
+        ("income", "Income"),
+        ("expense", "Expense"),
+        ("investment", "Investment"),
+    ]
 
     id = models.UUIDField(
         default=uuid.uuid4, editable=False, unique=True, primary_key=True
     )
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    name = models.CharField(max_length=150, blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-
-
-class ExpenseSource(models.Model):
-    """
-    Model representing the expense source for a user.
-    """
-
-    id = models.UUIDField(
-        default=uuid.uuid4, editable=False, unique=True, primary_key=True
+    transaction_type = models.CharField(
+        max_length=20, choices=TRANSACTION_CHOICES, default="Income"
     )
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    name = models.CharField(max_length=150, blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-
-
-class InvestmentSource(models.Model):
-    """
-    Model representing the investment source for a user.
-    """
-
-    id = models.UUIDField(
-        default=uuid.uuid4, editable=False, unique=True, primary_key=True
-    )
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    name = models.CharField(max_length=150, blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Income(models.Model):
-    """
-    Model representing income for a user.
-    """
-
-    id = models.UUIDField(
-        default=uuid.uuid4, editable=False, unique=True, primary_key=True
-    )
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    income_name = models.CharField(max_length=150)
-    income_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_name = models.CharField(max_length=150)
+    transaction_amount = models.DecimalField(max_digits=10, decimal_places=2)
     date_created = models.DateField(auto_now_add=True)
     date_modified = models.DateField(auto_now=True)
-    future_income_date = models.DateField("Future income date", blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.income_name} - {self.income_amount}"
-
-
-class Expense(models.Model):
-    """
-    Model representing expense for a user.
-    """
-
-    id = models.UUIDField(
-        default=uuid.uuid4, editable=False, unique=True, primary_key=True
-    )
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    expense_name = models.CharField(max_length=150)
-    expense_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    date_created = models.DateField(auto_now_add=True)
-    date_modified = models.DateField(auto_now=True)
-    future_expense_date = models.DateField("Future expense date", blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.expense_name} - {self.expense_amount}"
-
-
-class Investment(models.Model):
-    """
-    Model representing investment for a user.
-    """
-
-    id = models.UUIDField(
-        default=uuid.uuid4, editable=False, unique=True, primary_key=True
-    )
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    investment_name = models.CharField(max_length=150)
-    investment_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    date_created = models.DateField(auto_now_add=True)
-    date_modified = models.DateField(auto_now=True)
-    future_investment_date = models.DateField(
-        "Future investment date", blank=True, null=True
+    future_transaction_date = models.DateField(
+        "Future transaction date", blank=True, null=True
     )
 
     def __str__(self):
-        return f"{self.investment_name} - {self.investment_amount}"
+        return f"{self.transaction_name} - {self.transaction_amount}"
--- budget/views.py
+++ budget/views.py
@@ -1,226 +1,70 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import JsonResponse
-from .forms import IncomeForm, ExpenseForm, InvestmentForm
-from .models import (
-    Income,
-    Expense,
-    Investment,
-    IncomeSource,
-    ExpenseSource,
-    InvestmentSource,
-)
-from django.views.generic import View, TemplateView
-from django.http import HttpResponseNotAllowed
 from django.db.models import Sum
+from django.views import View
 
 
-class BudgetView(LoginRequiredMixin, View):
+from .models import Transaction
+from .forms import TransactionForm
+
+
+class TransactionView(View):
     template_name = "budget/budget.html"
-    income_form_class = IncomeForm
-    expense_form_class = ExpenseForm
-    investment_form_class = InvestmentForm
-
-    def get_total_amounts(self, user):
-        incomes = Income.objects.filter(user=user)
-        expenses = Expense.objects.filter(user=user)
-        investments = Investment.objects.filter(user=user)
-
-        total_income = sum(income.income_amount for income in incomes)
-        total_expenses = sum(expense.expense_amount for expense in expenses)
-        total_investments = sum(
-            investment.investment_amount for investment in investments
-        )
-
-        income_data = incomes.values("income_name").annotate(total=Sum("income_amount"))
-
-        expense_data = expenses.values("expense_name").annotate(
-            total=Sum("expense_amount")
-        )
-
-        investment_data = investments.values("investment_name").annotate(
-            total=Sum("investment_amount")
-        )
-
-        return (
-            total_income,
-            total_expenses,
-            total_investments,
-            incomes,
-            expenses,
-            investments,
-            income_data,
-            expense_data,
-            investment_data,
-        )
 
     def get(self, request):
-        user = request.user
-        (
-            total_income,
-            total_expenses,
-            total_investments,
-            incomes,
-            expenses,
-            investments,
-            income_data,
-            expense_data,
-            investment_data,
-        ) = self.get_total_amounts(user)
+        form = TransactionForm()
+        transactions = Transaction.objects.filter(user=request.user).order_by(
+            "transaction_type"
+        )
+        income_total = (
+            transactions.filter(transaction_type="income").aggregate(
+                Sum("transaction_amount")
+            )["transaction_amount__sum"]
+            or 0
+        )
 
-        remaining_balance = total_income - total_expenses - total_investments
+        expense_total = (
+            transactions.filter(transaction_type="expense").aggregate(
+                Sum("transaction_amount")
+            )["transaction_amount__sum"]
+            or 0
+        )
 
-        income_sources = IncomeSource.objects.filter(user=user)
-        income_form = self.income_form_class(user=user)
-        income_form.fields["income_name"].queryset = income_sources
-
-        expense_sources = ExpenseSource.objects.filter(user=user)
-        expense_form = self.expense_form_class(user=user)
-        expense_form.fields["expense_name"].queryset = expense_sources
-
-        investment_sources = InvestmentSource.objects.filter(user=user)
-        investment_form = self.investment_form_class(user=user)
-        investment_form.fields["investment_name"].queryset = investment_sources
-
+        investment_total = (
+            transactions.filter(transaction_type="investment").aggregate(
+                Sum("transaction_amount")
+            )["transaction_amount__sum"]
+            or 0
+        )
         context = {
-            "incomes": incomes,
-            "expenses": expenses,
-            "investments": investments,
-            "income_form": income_form,
-            "expense_form": expense_form,
-            "investment_form": investment_form,
-            "total_income": total_income,
-            "total_expenses": total_expenses,
-            "total_investments": total_investments,
-            "remaining_balance": remaining_balance,
-            "income_data": income_data,
-            "expense_data": expense_data,
-            "investment_data": investment_data,
+            "form": form,
+            "transactions": transactions,
+            "income_total": income_total,
+            "expense_total": expense_total,
+            "investment_total": investment_total,
         }
         return render(request, self.template_name, context)
 
     def post(self, request):
-        user = request.user
-
-        if "income_submit" in request.POST:
-            income_form = IncomeForm(request.POST, user=user)
-            if income_form.is_valid():
-                income = income_form.save(commit=False)
-                income.user = request.user
-
-                selected_income_source_id = request.POST.get("income_name")
-                if selected_income_source_id == "custom":
-                    custom_income_name = request.POST.get("custom_income")
-                    if custom_income_name:
-                        new_income_source = IncomeSource.objects.create(
-                            user=user, name=custom_income_name
-                        )
-                        income.income_name = new_income_source
-                else:
-                    selected_income_source = get_object_or_404(
-                        IncomeSource, id=selected_income_source_id
-                    )
-                    income.income_name = selected_income_source
-
-                income.save()
-                return redirect("budget")
-
-        elif "expense_submit" in request.POST:
-            expense_form = ExpenseForm(request.POST, user=user)
-            if expense_form.is_valid():
-                expense = expense_form.save(commit=False)
-                expense.user = request.user
-
-                selected_expense_source_id = request.POST.get("expense_name")
-                if selected_expense_source_id == "custom":
-                    custom_expense_name = request.POST.get("custom_expense")
-                    if custom_expense_name:
-                        new_expense_source = ExpenseSource.objects.create(
-                            user=user, name=custom_expense_name
-                        )
-                        expense.expense_name = new_expense_source
-                else:
-                    selected_expense_source = get_object_or_404(
-                        ExpenseSource, id=selected_expense_source_id
-                    )
-                    expense.expense_name = selected_expense_source
-
-                expense.save()
-                return redirect("budget")
-
-        elif "investment_submit" in request.POST:
-            investment_form = InvestmentForm(request.POST, user=user)
-            if investment_form.is_valid():
-                investment = investment_form.save(commit=False)
-                investment.user = request.user
-
-                selected_investment_source_id = request.POST.get("investment_name")
-                if selected_investment_source_id == "custom":
-                    custom_investment_name = request.POST.get("custom_investment")
-                    if custom_investment_name:
-                        new_investment_source = InvestmentSource.objects.create(
-                            user=user, name=custom_investment_name
-                        )
-                        investment.investment_name = new_investment_source
-                else:
-                    selected_investment_source = get_object_or_404(
-                        InvestmentSource, id=selected_investment_source_id
-                    )
-                    investment.investment_name = selected_investment_source
-
-                investment.save()
-                return redirect("budget")
-
-
-class DeleteIncomeView(LoginRequiredMixin, View):
-    def delete(self, request, income_id):
-        income = get_object_or_404(Income, id=income_id)
-        if income.user == request.user:
-            income.delete()
-            return JsonResponse({"message": "Income deleted successfully"})
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.user = request.user
+            transaction.save()
+            return redirect("budget")
         else:
-            return HttpResponseNotAllowed(["DELETE"])
+            transactions = Transaction.objects.filter(user=request.user)
 
-
-class DeleteExpenseView(LoginRequiredMixin, View):
-    def delete(self, request, expense_id):
-        expense = get_object_or_404(Expense, id=expense_id)
-        if expense.user == request.user:
-            expense.delete()
-            return JsonResponse({"message": "Expense deleted successfully"})
-        else:
-            return HttpResponseNotAllowed(["DELETE"])
-
-
-class DeleteInvestmentView(LoginRequiredMixin, View):
-    def delete(self, request, investment_id):
-        investment = get_object_or_404(Investment, id=investment_id)
-        if investment.user == request.user:
-            investment.delete()
-            return JsonResponse({"message": "Investment deleted successfully"})
-        else:
-            return HttpResponseNotAllowed(["DELETE"])
-
-
-class GetUpdatedTotalsView(LoginRequiredMixin, View):
-    def get(self, request):
-        user = request.user
-        incomes = Income.objects.filter(user=user)
-        expenses = Expense.objects.filter(user=user)
-        investments = Investment.objects.filter(user=user)
-
-        total_income = sum(income.income_amount for income in incomes)
-        total_expenses = sum(expense.expense_amount for expense in expenses)
-        total_investments = sum(
-            investment.investment_amount for investment in investments
-        )
-        remaining_balance = total_income - total_expenses - total_investments
-
-        return JsonResponse(
-            {
-                "total_income": total_income,
-                "total_expenses": total_expenses,
-                "total_investments": total_investments,
-                "remaining_balance": remaining_balance,
+            context = {
+                "form": form,
+                "transactions": transactions,
             }
-        )
+            return render(request, self.template_name, context)
+
+
+class DeleteTransactionView(View):
+    def post(self, request, id):
+        transaction = get_object_or_404(Transaction, pk=id, user=request.user)
+
+        if transaction.user == request.user:
+            transaction.delete()
+        return redirect("budget")
