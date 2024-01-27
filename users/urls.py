11f3a0d1f7ce49b01cfa0d3e4268ee730166e2fb
Ethan Diedericks <diedericksethan@gmail.com>
2024-01-27 23:43:31 +0200

    added guest log in

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import SignUpView
+from .views import SignUpView, guest_login
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("guest-login/", guest_login, name="guest_login"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import CreateView
-from django.contrib.auth import get_user_model
+from django.contrib import messages
+from django.contrib.auth import get_user_model, login
 from django.urls import reverse_lazy
 from .forms import CustomUserCreationForm
+from .managers import CustomUserManager
 
 
 class SignUpView(CreateView):
@@ -10,3 +12,16 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     template_name = "registration/signup.html"
     success_url = reverse_lazy("login")
+
+
+def guest_login(request):
+    user = get_user_model().objects.filter(email="guest@gmail.com").first()
+
+    if user:
+        login(request, user)
+    else:
+        user = CustomUserManager.create_user("guest@gmail.com", "Binary7j*")
+        login(request, user)
+
+    messages.success(request, "Guest user logged in successfully.")
+    return redirect("budget")
